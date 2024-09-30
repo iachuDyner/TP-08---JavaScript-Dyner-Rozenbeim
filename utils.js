bc6712c9b1b070fbf857d496df70f8a7a3123840
iachuDyner <48313765@est-ort.edu.ar>
2024-09-30 09:56:24 -0300

    terminado triangulo de asteriscos

--- utils.js
+++ utils.js
@@ -36,3 +36,29 @@ const numerosDobles = numeros.map(numeros => numeros*2)
 
 console.log(numerosDobles)
 }
+
+function DibujoUno()
+{
+    dibujo = `*`
+    console.log(dibujo)
+    console.log(dibujo.repeat(2))
+    console.log(dibujo.repeat(3))
+    console.log(dibujo.repeat(4))
+    console.log(dibujo.repeat(5))
+}
+function DibujoDos()
+{
+    let linea1 = '-'.repeat(4) + '*' + '-'.repeat(4) 
+    let linea2 = '-'.repeat(3) + '*'.repeat(3)+ '-'.repeat(3)
+    let linea3 = '-'.repeat(2) + '*'.repeat(5)+ '-'.repeat(2)
+    let linea4 = '-' + '*'.repeat(7)+ '-'
+    let linea5 = '*'.repeat(9)
+
+
+    console.log(linea1)
+    console.log(linea2)
+    console.log(linea3)
+    console.log(linea4)
+    console.log(linea5)
+    
+}
